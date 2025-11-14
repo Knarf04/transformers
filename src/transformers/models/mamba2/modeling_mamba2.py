0d9ad04948ef482da2c8ebd3636781e00c11a32f
Haochen Shen <shenhaochen2000@gmail.com>
2025-11-14 10:08:24 -0600

    update

--- src/transformers/models/mamba2/modeling_mamba2.py
+++ src/transformers/models/mamba2/modeling_mamba2.py
@@ -314,6 +314,7 @@ class Mamba2Mixer(nn.Module):
         self.scale_portion = self.experiments.get("scale_portion", 0.95)
 
         self.mmd = self.experiments.get("mmd", False)
+        self.distribution_reg = self.experiments.get("distribution_reg", False)
 
         if not is_fast_path_available:
             logger.warning_once(
@@ -517,6 +518,21 @@ class Mamba2Mixer(nn.Module):
                         if self.logits_num >= 5:
                             self.logits_reg = False
 
+                if self.distribution_reg:
+                    # (batch, seqlen, nheads)
+                    dtype = dt.dtype
+                    dt_sp = nn.functional.softplus((dt + self.dt_bias).to(dtype=torch.float32)).to(dtype=dtype)
+                    forget = torch.exp(A * dt_sp)
+                    record = {
+                        "layer_idx": self.layer_idx,
+                        "dt": dt_sp.tolist(),
+                        "forget": forget.tolist()
+                        }
+                    filename = "/gpfs/hshen/mmd/mamba2.jsonl"
+                    os.makedirs(os.path.dirname(filename), exist_ok=True)
+                    with open(filename, "a", encoding="utf-8") as f:
+                        f.write(json.dumps(record) + '\n')
+                    
                 if self.mmd:
                     B_view = B.view(batch_size, seq_len, self.n_groups, -1)
                     C_view = C.view(batch_size, seq_len, self.n_groups, -1)
